c49d81fe0876270a02d7ab1605072d9f475ee6cf
Li-Guangyao <g-li@foxmail.com>
2022-10-21 01:23:53 +0800

    完成界面初始化

---
+++ src/mainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <div className="side-bar"></div>
+      <div className="info-bar"></div>
+        <div className='chat-panel'></div>
+    </div>
+  );
+}
+
+export default Main;
