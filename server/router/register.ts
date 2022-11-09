217e0b25d27703f90dba81280df8c5c803d94827
Li-Guangyao <g-li@foxmail.com>
2022-11-09 11:21:52 +0800

    update

--- server/router/register.ts
+++ server/router/register.ts
@@ -1,43 +1,56 @@
 import { Router, Response, Request } from "express";
-import redisManager from '../manager/redisManager'
+import redisManager from "../manager/redisManager";
 
 const emailManager = require("../manager/emailManager.js");
 const model = require("../models/model.js");
 const router: Router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  let data = req.body;
-  let { username, password } = data;
+  const data = req.body;
+  const { username, password } = data;
   res.send(username + password);
 });
 
 router.post("/send_code", async (req: Request, res: Response) => {
-  let data = req.body;
-  let { email } = data;
+  const data = req.body;
+  const { email } = data;
   sendCode(email);
+  res.json({
+    tip: "Send a authorization code.",
+    status: 200,
+  });
 });
 
 router.post("/verify_code", async (req: Request, res: Response) => {
-  let data = req.body;
-  let { code } = data;
-  res.send(await verifyCode(code))
+  const data = req.body;
+  const { email, code } = data;
+  res.json({
+    status: await verifyCode(email, code),
+  });
 });
 
 function sendCode(email: string): void {
-  let code = generateCode();
+  const code = generateCode();
   emailManager.sendMail(email, code);
-  redisManager.set("username", code);
+  const codeObj = {
+    code,
+    expireTime: Date.now() + 1000 * 60 * 2,
+  };
+  redisManager.set(email, JSON.stringify(codeObj));
 }
 
 function generateCode(): string {
-  let res = "" + Math.round(Math.random() * 1000000);
+  const res = "" + Math.round(Math.random() * 1000000);
   return res;
 }
 
-async function verifyCode(usercode:string) :Promise<any>{
-  console.log("Request verify Code")
-  const code = await redisManager.get("username")
-  return code==usercode
+async function verifyCode(email: string, code: string): Promise<any> {
+  const codeObj = JSON.parse(await redisManager.get(email));
+  if (codeObj && Date.now() < codeObj.expireTime) {
+    return codeObj.code == code;
+  } else {
+    return false;
+  }
 }
 
 export default router;
